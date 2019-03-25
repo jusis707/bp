f9cf3e7f5d4d7f0fb26edf4bc94bef67cba5a3f8
jusis707 <48894008+jusis707@users.noreply.github.com>
2019-03-25 11:43:20 +0200

    Update server.py

--- server.py
+++ server.py
@@ -25,6 +25,6 @@ httpd.serve_forever()
 
 import datetime
 
-with open("scr.txt", mode='a') as file:
+with open("/mnt/scr.txt", mode='a') as file:
     file.write('Printed string %s recorded at %s.\n' % 
                (scr, datetime.datetime.now()))
