d81acdcc95312d36b5fad43759d6d285742abca6
jusis707 <48894008+jusis707@users.noreply.github.com>
2019-03-25 16:46:10 +0200

    Update index.py

--- index.py
+++ index.py
@@ -3,5 +3,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+    fo= open("test.txt", "w")
+    filebuffer = ["brave new world"]
+    fo.writelines(filebuffer)
+    fo.close()
+    
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
