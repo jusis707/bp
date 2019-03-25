6c392da06fd3e738c165e4bcf52ed9999db7c590
jusis707 <48894008+jusis707@users.noreply.github.com>
2019-03-25 11:54:29 +0200

    Update server.py

--- server.py
+++ server.py
@@ -23,8 +23,20 @@ httpd = TCPServer(("", PORT), Handler)
 httpd.serve_forever()
 
 
-import datetime
+from datetime import date
 
-with open("/mnt/scr.txt", mode='a') as file:
-    file.write('Printed string %s recorded at %s.\n' % 
-               (scr, datetime.datetime.now()))
+def get_filename_datetime():
+    # Use current date to get a text file name.
+    return "file-" + str(date.today()) + ".txt"
+
+# Get full path for writing.
+name = get_filename_datetime()
+print("NAME", name)
+
+path = "/mnt/" + name
+print("PATH", path);
+
+with open(path, "w") as f:
+    # Write data to file.
+    f.write("HELLO\n")
+    f.write("WORLD\n")
