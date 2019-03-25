2a8775cf31e60f975bc1c1cdab92032c75cbea4f
jusis707 <48894008+jusis707@users.noreply.github.com>
2019-03-25 18:25:23 +0200

    Update writer.py

--- writer.py
+++ writer.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from datetime import datetime
+from threading import threading
 
 app = Flask(__name__, static_url_path='')
 
@@ -10,6 +11,9 @@ def log_feedback():
         print(request.data.decode("utf-8"))
         fo.write('\n')
     return 'Got it!'
+    threading.Timer(10.0, hello_world).start() 
+      with open("/mnt/timestamp.txt","a") as fo:
+        fo.write('rrrrrrrrrr\n')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug = False)
