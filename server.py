5f6c602ea71dc6c502e0dd9e33ad151ab7b66121
jusis707 <48894008+jusis707@users.noreply.github.com>
2019-03-25 14:53:27 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,25 +1,11 @@
-from http.server import HTTPStatus, BaseHTTPRequestHandler
-from socketserver import TCPServer
+from flask import Flask
 
-PORT = 8000
-MESSAGE = "Hello, world!\n".encode("ascii")
+app = Flask(__name__)
 
-
-class Handler(BaseHTTPRequestHandler):
-    """Respond to requests with hello."""
-
-    def do_GET(self):
-        """Handle GET"""
-        self.send_response(HTTPStatus.OK)
-        self.send_header("Content-type", "text/plain")
-        self.send_header("Content-length", len(MESSAGE))
-        self.end_headers()
-        self.wfile.write(MESSAGE)
+@app.route("/")
+def hello():
+    return "Flask inside Docker!!"
 
 
-print("Serving at port", PORT)
-TCPServer.allow_reuse_address = True
-httpd = TCPServer(("", PORT), Handler)
-httpd.serve_forever()
-
-print("This line will be printed.")
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0')
