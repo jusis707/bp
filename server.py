eac5e80ae20c037ec4547ed2d5558f2fd82ec44b
jusis707 <48894008+jusis707@users.noreply.github.com>
2019-03-25 14:29:55 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,24 @@
-import time
-starttime=time.time()
-while True:
-  print "Hello world"
-  time.sleep(3.0 - ((time.time() - starttime) % 3.0))
+from http.server import HTTPStatus, BaseHTTPRequestHandler
+from socketserver import TCPServer
+
+PORT = 8000
+MESSAGE = "Hello, world!\n".encode("ascii")
+
+
+class Handler(BaseHTTPRequestHandler):
+    """Respond to requests with hello."""
+
+    def do_GET(self):
+        """Handle GET"""
+        self.send_response(HTTPStatus.OK)
+        self.send_header("Content-type", "text/plain")
+        self.send_header("Content-length", len(MESSAGE))
+        self.end_headers()
+        self.wfile.write(MESSAGE)
+
+
+print("Serving at port", PORT)
+TCPServer.allow_reuse_address = True
+httpd = TCPServer(("", PORT), Handler)
+httpd.serve_forever()
+
