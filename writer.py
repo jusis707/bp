a7f69c57104e68e7b6abd1ba500c1ec9a9b1bed1
jusis707 <48894008+jusis707@users.noreply.github.com>
2019-03-25 18:50:30 +0200

    Update writer.py

--- writer.py
+++ writer.py
@@ -1,17 +1,17 @@
 from flask import Flask, request, jsonify
 from datetime import datetime
-
+from threading import threading
 
 app = Flask(__name__, static_url_path='')
 
 @app.route('/writetofile', methods=['POST']) 
-def log_feedback():
-    with open("/mnt/hello-bp.txt","a") as fo:
-        fo.write(request.data.decode("utf-8"))
-        print(request.data.decode("utf-8"))
-        fo.write('\n')
-    return 'Got it!'
+def printit():
+  threading.Timer(5.0, printit).start()
+  f=open("/mnt/acs.txt",'a')
+  f.write ("heloo"+'\t')
+  f.write(datetime.datetime.now().ctime())
 
+printit()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug = False)
