83fe05a6f365fbe297141b23f4349f8f3183cb0b
jusis707 <48894008+jusis707@users.noreply.github.com>
2019-03-25 16:09:48 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,7 @@
-print('Hello, world!')
+import datetime
+
+def timeStamped(fname, fmt='%Y-%m-%d-%H-%M-%S_{fname}'):
+    return datetime.datetime.now().strftime(fmt).format(fname=fname)
+
+with open(timeStamped('myfile.txt'),'w') as outf:
+    outf.write('HELLO BP!')
