49a92341f5c3ed760575631cbc3b0e5f15db4852
jusis707 <48894008+jusis707@users.noreply.github.com>
2019-03-27 19:17:48 +0200

    Update writer.py

--- writer.py
+++ writer.py
@@ -2,7 +2,7 @@ from flask import Flask
 import datetime
 import threading
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 def printit():
   threading.Timer(10.0, printit).start()
@@ -16,4 +16,4 @@ def hello():
     return "Hello BP!"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug = False)
