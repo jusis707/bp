f43c5ae56d1b39c1fc62119d26905a225f23a49d
jusis707 <48894008+jusis707@users.noreply.github.com>
2019-03-27 08:30:51 +0200

    Update writer.py

--- writer.py
+++ writer.py
@@ -1,4 +1,3 @@
-from flask import Flask, request, jsonify
 from datetime import datetime
 import datetime
 import threading
@@ -6,17 +5,14 @@ import logging
 
 app = Flask(__name__, static_url_path='')
 
-def printit():
-  threading.Timer(10.0, printit).start()
-  f=open("/mnt/stamp.txt",'a')
-  f.write ("\nHello-BP\n")
+@@ -12,5 +13,10 @@ def printit():
   f.write(datetime.datetime.now().ctime())
 printit()
 
 @app.route('/hello')
 def printMsg():
     app.logger.info('testing info log')
-    return "HELLO-BP!"  
+    return "HELLO-BP!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug = False)
